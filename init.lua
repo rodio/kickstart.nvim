b5add087d932fd4d3c6f508cc74d0ac238b20d54
Rodion Borovyk <rodion.borovyk@gmail.com>
2024-05-07 16:42:48 +0200

    Add leptosfmt

--- init.lua
+++ init.lua
@@ -568,7 +568,17 @@ require('lazy').setup({
         clangd = {},
         -- gopls = {},
         pyright = {},
-        rust_analyzer = {},
+        rust_analyzer = {
+          settings = {
+            ['rust-analyzer'] = {
+              rustfmt = {
+                -- TODO: try https://github.com/folke/neoconf.nvim
+                -- https://github.com/bram209/leptosfmt?tab=readme-ov-file#using-with-rust-analyzer
+                overrideCommand = { 'leptosfmt', '--stdin', '--rustfmt' },
+              },
+            },
+          },
+        },
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
