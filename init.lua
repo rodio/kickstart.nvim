856759ab85368ec9bb7b5ddfe0f4be5a4162b346
Rodion Borovyk <rodion.borovyk@gmail.com>
2024-05-08 21:31:42 +0200

    pretty gitsigns

--- init.lua
+++ init.lua
@@ -249,11 +249,12 @@ require('lazy').setup({
     'lewis6991/gitsigns.nvim',
     opts = {
       signs = {
-        add = { text = '+' },
-        change = { text = '~' },
+        add = { text = '┃' },
+        change = { text = '┃' },
         delete = { text = '_' },
         topdelete = { text = '‾' },
         changedelete = { text = '~' },
+        untracked = { text = '┆' },
       },
     },
   },
